7d54a333639dde78d03a30a8c334faf474fecac2
elephantnose <32924900+ElephantNose@users.noreply.github.com>
2018-01-26 16:05:03 +0800

    write2Excel

---
+++ write2xls.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+# i=0 为第一行
+# j=0 为第一列 
+
+from xlwt import *
+f = Workbook(encoding='utf-8')  # 打开文件
+table = f.add_sheet('record')  # 创建表
+
+with open('./13', 'r') as data:
+    contents_list = data.readlines()
+
+for i, contents in enumerate(contents_list):
+    content = contents.split('\t')
+    for j, everydata in enumerate(content):
+        table.write(i, j , everydata)
+
+f.save('record.xls')  # 保存文件
+
